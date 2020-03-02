38987cba2364947b2427eea6bb575bec77b88842
shrivatsaBhat <52356082+shrivatsaBhat@users.noreply.github.com>
2020-03-02 22:47:32 +0530

    Word Split Added
    The problem due to the word split during the next line is resolved.
    The plotter can split the word depending on the *oneline*  value.

--- dictowriter.py
+++ dictowriter.py
@@ -1,29 +1,55 @@
 import os
 import time
 import threading
-import s2t_v4 as s2t
+#import s2t_v4 as s2t
+from gtts import gTTS
+#import tessocr
+import s2t
 import serial
 import time
-"sending PYTHON 2 CODE ported to py3"
+
+speech_mode = True
+ttsfeedback = True
+
+
 # Open grbl serial port
-s = serial.Serial('/dev/ttyACM1',115200)
-#buffer = "welcome to project dicto-writer 1.0"
-buffer = " "
-oneline = 30
-scale = 0.3 # goes well with line yoffset drop of 10
+try:
+    s = serial.Serial('/dev/ttyACM0',115200)
+    print("arduino port: ","/dev/ttyACM0")
+except:
+    s = serial.Serial('/dev/ttyACM1',115200)
+    print("arduino port: ","/dev/ttyACM1")
+
+" buffer is a keyword in python for some reason .."
+
+#buff = "welcome to project dicto-writer 1.0"
+buff = " "
+oneline = 40 #characters that can be writen in a line
+current_line = 0
+line_drop = 10
+# scale = 0.3 # goes well with line yoffset drop of 10
+scale = 0.25 # also tweak yoffset
+
+#font = "scriptc" #doble stroke
+font = "scripts"
+#font = "rowmans"
+
 cmd = "./h2g/src/hf2gcode" # ./prog -params "text"
-gcd_file = "write.txt"
-current_line = 40
+gcd_folder = "linegcodes/" # use / after folder
+gcd_filename = "line"
+
+os.system(f"rm -rf {gcd_folder}") #flush out revious gcodes from storage
+os.system(f"mkdir {gcd_folder}")
+# s2t.energy_threshold = 2000
 
 from collections import deque
 plotter_queue = []
-plotter_queue = deque(plotter_queue)
+plotter_queue = deque(plotter_queue) # to store pending line.gc filenames 
 
-
-
-def gc_sender(newgcode):
+def stream_gcode(filename):
     # Open g-code file
-    f = newgcode
+    f = open(filename,'r')
+
     # Wake up grbl
     s.write("\r\n\r\n".encode())
     time.sleep(2)   # Wait for grbl to initialize 
@@ -32,65 +58,106 @@ def gc_sender(newgcode):
     # Stream g-code to grbl
     for line in f:
         l = line.strip() # Strip all EOL characters for consistency
-        print('Sending: ' + l)
+        #print('Sending: ' + l)
         ln_cmd = l + '\n'
         s.write(ln_cmd.encode()) # Send g-code block to grbl
         grbl_out = s.readline() # Wait for grbl response with carriage return
-        print(grbl_out.strip())
+        #print(grbl_out.strip())
 
     # Wait here until grbl is finished to close serial port and file.
     print("finished")
 
     # Close file and serial port
-    #f.close()
-    #s.close()    
+    f.close()
+    
+    #s.close() # dont know whether closing and opening of port is required ... might be redundant   
 
-    # -----
-
-# s2t.energy_threshold = 2000
-
-
-def speech_recog():
-    while(len(buffer) <= 30):
-        new_msg = input("write something:")
-        buffer + new_msg
-
-def gen_gcode(mesg,x,y):
+def gen_gcode(mesg,x,y,linefn):
     # print(scale)
-    print("generating GCODE")
-    x,y = str(x),str(y)
+    print("Generating gcode for: ",mesg)
+    print(f"Writing to {linefn}{gcd_filename}")
+    x,y,lineno = str(x),str(y),str(linefn)
     sca = str(scale)
-    bashout = cmd +" -m -o " +gcd_file+" --scale "+sca+" -x "+ x +" -y "+ y + " " + "\"" +mesg +"\""
+    generatecmd = cmd +" -m --font "+font+" -o "+gcd_folder+linefn+" --scale "+sca+" -x "+ x +" -y "+ y + " " + "\"" +mesg +"\""
     # bashout = cmd +" --scale "+sca+" -x "+ x +" -y "+ y + " " + "\"" +mesg +"\""
     # print(bashout)
-    return os.system(bashout)
+    return os.system(generatecmd)
 
-def run_plotter(filename):
-    print("Plotter is writing :\n"+buffer)
-    # cmd2 = "gcd -c v3.json -f write.txt"
-    
-    
+def run_plotter():
+    while True:
+        global plotter_queue
+        if (len(plotter_queue) >= 1):
+            cur_line_file = plotter_queue.popleft() # pop first line ,then second, and so on..
+            print("Plotter is writing :"+cur_line_file)
+            stream_gcode(f"{gcd_folder}{cur_line_file}")
+            # cmd2 = "gcd -c v3.json -f write.txt" #deprecated bcz not working as expected
+        else:
+            # print("No pending gcodes ..",end=" ")
+            #print(".",end=" ")
+            pass
+        time.sleep(1)
 
+" TAKEN CARE BY MAIN THREAD "
 ## need to use one thread to fill the buffer by speech recognition
 # speech_t = threading.Thread(target = speech_recog)
 # speech_t.start()
-
-" thread to control plotter queue "
-threading.Thread(target=run_plotter,deamon=True).start()
+" TAKEN CARE BY MAIN THREAD "
 
 def main():
-    while(True):
-        if(len(buffer) >= oneline):
-            gen_gcode(buffer[:oneline],0,current_line) #text , xoffset , yoffset
-            current_line -= 10 #step down a line
-            run_plotter("write.txt") # pass the gcode
-            buffer = buffer[oneline:] #retain remaining buffer text
-        # speech_recog()
-        #time.sleep(0.5) # simply
-        #buffer += input("fill buffer :") # fills buffer using user input
-        voice = s2t.recog()
-        if voice is not None:
-            buffer += voice # 
+    global buff
+    global current_line
+    global oneline
+    
+    while True:
+        if(len(buff) >= oneline):
+            offset_txt = None
+            #check for the first space from the end of buff
+            if buff[oneline-1] == " " or buff[oneline] == " ": #check current and next lettr is space?
+                print_txt = buff[:oneline]
+                buff = buff[oneline:]  # retain remaining buff text
+
+            else:
+                index = 0
+                for i in range(len(buff[:oneline])-1, -1, -1): #check for space from last 
+                    index += 1
+                    if buff[i] == " ":
+                        print_txt = buff[:len(buff[:oneline])-index] #send to gcd conveter
+                        buff = buff[oneline-index:]  # retain remaining buff text
+                        break
+            #wordend = buff.find(' ')
+            linefn = f"{gcd_filename}{current_line}.gc" #linefilename
+            gen_gcode(print_txt,0,current_line,linefn) #text , xoffset , yoffset 
+            #gen_gcode will save and store gcode for each line in a new file
+            #print("printing ->" + print_txt)
+            plotter_queue.append(linefn)
+
+            current_line -= line_drop #step down a line
+            
+            #buff = buff[oneline:] # retain remaining buff text
+            #print("buffer ->" + buff)
+        else:
+            if not speech_mode:
+                newtext = input("enter text:")+" "
+            else:
+                newtext = s2t.recog()
+                print("You said:",newtext)
+                #allows user to listen to recognized audio using tts 
+                if ttsfeedback and newtext is not None:
+                    fbaud = gTTS(newtext)
+                    fbaud.save("ttsaud.mp3")
+                    os.system("omxplayer ttsaud.mp3")
+                    if input("Is it correct (y/n):") == 'n':
+                        print("Rejected:",newtext)
+                        newtext = None
+                    else:
+                        pass
+                if newtext is not None:
+                    buff += ' '+newtext # append into buff
+
+def plotter_handler():
+	" thread to control plotter queue "
+	threading.Thread(target=run_plotter,daemon=True).start()
 
 if __name__ == "__main__":
-    main()
+	plotter_handler() # sequentialy serial_stream each new file generated in gcd_folder
+	main() #main thread performs continous speech recog and gcode generation
